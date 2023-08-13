6022d37d5bbc63c5e525809aa204abad7118878b
NQTrung08 <nqtrung0810@gmail.com>
2023-08-14 03:39:02 +0700

    end

--- 8. Xây dựng web The band/custom.js
+++ 8. Xây dựng web The band/custom.js
@@ -35,3 +35,49 @@ modalContainer.addEventListener('click', function(event) {
 
 })
 
+
+/////////////////// MOBILE - MENU //////////////////////////
+
+var header = document.getElementById("header")
+
+var mobileMenu = document.getElementById('mobile-menu')
+
+// lưu css header height bằng 1 biến
+var headerHeigth = header.clientHeight;
+
+//____ Lắng nghe click vào mobileMenu ____________//
+mobileMenu.onclick = function(){
+
+    // so sánh biến = 46 hay không trả về T hay F _____//
+    var isCLose = header.clientHeight === headerHeigth;
+    if (isCLose) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+
+    }
+}
+
+
+// Tự động đóng khi chọn menu_____//
+
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+// lắng nghe click vào thẻ a để đóng menu và chuyển đến đó
+
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+
+    menuItem.onclick = function (event) {
+        
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if(!isParentMenu) {
+            header.style.height = null;
+
+        } else {
+            event.preventDefault();
+            // Bỏ việc trôi trang khi nhấn vào
+        }
+    }
+}
+
